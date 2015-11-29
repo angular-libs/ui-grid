1778337d3f416cc74e11992737cc075e69c3e5d6
Kuldeep Keshwar <kuldeepkeshwar@gmail.com>
2015-11-29 23:57:13 +0530

    added test case

--- test/spec/directives/ui-grid-filter.js
+++ test/spec/directives/ui-grid-filter.js
@@ -2,25 +2,67 @@
 
 describe('Directive: uiGridFilter', function () {
 
-  var scope,element;
+  var scope;
   beforeEach(module('ui.grid'));
   beforeEach(inject(function ($rootScope,$compile,$timeout) {
     scope=$rootScope.$new();
     scope.gridOptions={
-      src:[{id:1},{id:2}]
+      src:[{id:1,name:'a'},{id:2,name:'b'}]
     };
+    scope.nameFilterConfig={
+      key:'name',
+      filterFn:function(array,filter,value){
+        //console.log(array,filter,value);
+        for(var i = array.length - 1; i >= 0; i--) {
+          if(value && array[i][filter.key]!=value){
+            array.splice(i, 1);
+          }
+        }
+        //console.log(array);
+      }
+    }
     var body=angular.element(document.body);
-    element=angular.element('<div ui-grid="gridOptions"><input id="filter" ng-model="filter" ui-grid-filter="id"><div ui-grid-repeat var="item"><span ng-bind="item.id"></span></div></div>');
+    var element=angular.element(
+      '<div ui-grid="gridOptions">' +
+          '<input id="filter" ng-model="filter" ui-grid-filter="id">' +
+          '<input id="nameFilter" ng-model="nameFilter" ui-grid-filter="nameFilterConfig">' +
+          '<div ui-grid-repeat var="item">' +
+              '<span class="id" ng-bind="item.id"></span>' +
+              '<span class="name" ng-bind="item.name"></span>' +
+          '</div>' +
+      '</div>');
+    //body.html('');
     body.append(element);
     $compile(element)(scope);
     scope.$digest();
     $timeout.flush();
   }));
-  it('should check ui-grid-filter', inject(function () {
-    scope.filter=1;
-    scope.$digest();
-    var spans=element.find('span');
+  afterEach(function(){
+    var body=angular.element(document.body);
+    body.html('');
+  })
+  it('should filter with id', inject(function () {
+    var inputEl=angular.element(document.getElementById('filter'));
+    inputEl.val(1).triggerHandler('input');
+    var spans=angular.element(document.querySelectorAll('.id'));
     expect(spans.length).toBe(1);
-    expect(spans[0].innerHTML).toBe('1');
+    expect(spans.html()).toBe(inputEl.val());
+
+    inputEl.val('').triggerHandler('input');
+    spans=angular.element(document.querySelectorAll('.id'));
+    expect(spans.length).toBe(2);
+
+  }));
+  it('should filter with name', inject(function () {
+    var inputEl=angular.element(document.getElementById('nameFilter'));
+    inputEl.val('b').triggerHandler('input');
+
+    var spans=angular.element(document.querySelectorAll('.name'));
+    expect(spans.length).toBe(1);
+    expect(spans.html()).toBe(inputEl.val());
+
+    inputEl.val('').triggerHandler('input');
+    spans=angular.element(document.querySelectorAll('.name'));
+    expect(spans.length).toBe(2);
   }));
 });
--- test/spec/directives/ui-grid-sort.js
+++ test/spec/directives/ui-grid-sort.js
@@ -7,25 +7,62 @@ describe('Directive: uiGridSort', function () {
   beforeEach(inject(function ($rootScope,$compile,$timeout) {
     scope=$rootScope.$new();
     scope.gridOptions={
-      src:[{id:1},{id:2}]
+      src:[{id:1,name:'b'},{id:2,name:'a'}]
     };
+    scope.sorter={
+      key:'name',
+      order:1,
+      sortFn:function(array,sorter){
+        array.sort(function(a,b){
+          return (a.name< b.name)?sorter.order*1:sorter.order*-1;
+        });
+      }
+    }
     var body=angular.element(document.body);
-    element=angular.element('<div ui-grid="gridOptions"><p class="sorter" ui-grid-sort="id">Id</p><div ui-grid-repeat var="item"><span ng-bind="item.id"></span></div></div>');
+    element=angular.element('<div ui-grid="gridOptions">' +
+                              '<p class="id-sorter" ui-grid-sort="id">Id</p>' +
+                              '<p class="name-sorter" ui-grid-sort="sorter">Id</p>' +
+                              '<div ui-grid-repeat var="item">' +
+                                  '<span class="id" ng-bind="item.id"></span>' +
+                                  '<span  class="name" ng-bind="item.name"></span>' +
+                              '</div>' +
+                            '</div>');
     body.append(element);
     $compile(element)(scope);
     scope.$digest();
     $timeout.flush();
   }));
-  it('should check ui-grid-sorter', inject(function () {
-    element.find('p').triggerHandler('click');
-    var spans=element.find('span');
+  afterEach(function(){
+    angular.element(document.body).html('');
+  });
+  xit('should sort with id', inject(function () {
+    var sorterEl=angular.element(document.querySelector('.id-sorter'));
+    sorterEl.triggerHandler('click');
+
+    var spans=angular.element(document.querySelectorAll('.id'));
+
     expect(spans.length).toBe(2);
     expect(spans[0].innerHTML).toBe('2');
 
-    element.find('p').triggerHandler('click');
-    spans=element.find('span');
+    sorterEl.triggerHandler('click');
+    spans=angular.element(document.querySelectorAll('.id'));
+
     expect(spans.length).toBe(2);
     expect(spans[0].innerHTML).toBe('1');
+  }));
+  it('should sort with name', inject(function () {
+    var sorterEl=angular.element(document.querySelector('.name-sorter'));
+    sorterEl.triggerHandler('click');
+    var spans=angular.element(document.querySelectorAll('.name'));
+
+    expect(spans.length).toBe(2);
+    expect(spans[0].innerHTML).toBe('a');
+
+    sorterEl.triggerHandler('click');
+    spans=angular.element(document.querySelectorAll('.name'));
+
+    expect(spans.length).toBe(2);
+    expect(spans[0].innerHTML).toBe('b');
   }));
 
 });
